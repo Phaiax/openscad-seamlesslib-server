ee738005e70e2623dd4a567e51df23a36a2d8b5e
Phaiax <phaiax@invisibletower.de>
2013-08-27 23:37:31 +0200

    Add licensing info

--- src/web/models.py
+++ src/web/models.py
@@ -20,12 +20,14 @@ class Module(models.Model):
     created = models.DateTimeField(db_index=True, auto_now_add = True)
     finished = models.BooleanField(default = False)
     sourcecode = models.TextField(help_text="""
+        <strong>Commiting this sourcecode you irrevocably agree to release your contribution under the CC-BY-SA 3.0 License and the GPLv2.</strong>
+        <p>
         Some tips to make a good and reusable module:
         <ul><li>Don't use global variables! They can cause confusion if they override variables used in the calling script.</li>
         <li>If your sourcecode contains multiple modules, it's probably the best to move them into your main module (yes, that's possible).</li>
         <li>Use parameters to make your module more flexible.</li>
         <li>Parameters like spacing for bores or $fn for detailism are always welcome.</li>
-        </ul> 
+        </ul></p> 
     """)
     documentation = models.TextField(help_text = "You can use your own module name for examples or copy the new unique module name (~author-modulename-version) from above to simplify work for the users of your work.")
     tags = models.ManyToManyField(Tag, blank=True, related_name="tagged_modules")
